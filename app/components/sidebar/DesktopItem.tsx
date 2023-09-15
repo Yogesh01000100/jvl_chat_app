3f58780c1c69182375942d832a5a4ad1b1187165
Yogesh01000100 <yogeshone678@gmail.com>
2023-09-15 16:45:06 +0530

    side_bar

---
+++ app/components/sidebar/DesktopItem.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import clsx from "clsx";
+import Link from "next/link";
+
+interface DesktopItemProps {
+    label: string;
+    icon: any;
+    href: string;
+    onClick?: () => void;
+    active?: boolean;
+}
+
+const DesktopItem: React.FC<DesktopItemProps> = ({ label, icon, href, onClick, active }) => {
+    const handleClick = () => {
+        if (onClick) {
+            onClick();
+        }
+    }
+
+    return (
+        <li onClick={handleClick}>
+            <Link href={href}>
+                <span>{label}</span>
+            </Link>
+        </li>
+    )
+}
+
+export default DesktopItem;
