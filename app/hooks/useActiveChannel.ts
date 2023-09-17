a491ad5903a5f191fcc802f348653ea91c964fc2
Yogesh01000100 <yogeshone678@gmail.com>
2023-09-17 12:31:24 +0530

    active channel | fix

--- app/hooks/useActiveChannel.ts
+++ app/hooks/useActiveChannel.ts
@@ -11,7 +11,7 @@ const useActiveChannel = () => {
     let channel = activeChannel;
 
     if (!channel) {
-      channel = pusherClient.subscribe('presence-messenger');
+      channel = pusherClient.subscribe('user-presence');
       setActiveChannel(channel);
     }
 
@@ -32,7 +32,7 @@ const useActiveChannel = () => {
 
     return () => {
       if (activeChannel) {
-        pusherClient.unsubscribe('presence-messenger');
+        pusherClient.unsubscribe('user-presence');
         setActiveChannel(null);
       }
     }
